9a9cd1627766a5dbe140fd231d7db945c8eabeeb
Sarah Manning <sarahnmanning@gmail.com>
2018-07-05 18:48:37 -0400

    created separate colorBox component and moved the code to that file. Stripped down the app file to import the component file.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import ColorBox from './ColorBox';
 
 class App extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {
-      backgroundColor: 'blue',
-    };
-  }
-
-  red() {
-    this.setState({
-      backgroundColor: 'red',
-    })
-  }
-
-  green() {
-    this.setState({
-      backgroundColor: 'green',    })
-  }
-
+    
   render() {
-    const style = {
-            boxStyle: {
-              backgroundColor: this.state.backgroundColor,
-            }
-          };
-    const {boxStyle} = style;
     return (
-      <div className="App">
-          <span className="box" style={boxStyle}>{this.state.backgroundColor}</span>
-          <button onClick={this.red.bind(this)}> Red </button>
-          <button onClick={this.green.bind(this)}> Green </button>
+      <div>
+        < ColorBox />
       </div>
     );
   }
---
+++ src/ColorBox.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          backgroundColor: 'blue',
+        };
+      }
+    
+      red() {
+        this.setState({
+          backgroundColor: 'red',
+        })
+      }
+    
+      green() {
+        this.setState({
+          backgroundColor: 'green',   
+         })
+      }
+    
+    render() {
+        const style = {
+            boxStyle: {
+              backgroundColor: this.state.backgroundColor,
+            }
+          };
+        const {boxStyle} = style;
+        return (
+            <div className="App">
+                <span className="box" style={boxStyle}>{this.state.backgroundColor}</span>
+                <button onClick={this.red.bind(this)}> Red </button>
+                <button onClick={this.green.bind(this)}> Green </button>
+            </div>
+        )
+    }
+}
+
+export default ColorBox
